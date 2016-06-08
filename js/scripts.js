76a43df1dbd8235fc2563608f348555b7b7a81cf
Kevin VanEvery and Matthew Giem <kevin@>
2016-06-08 08:21:15 -0700

    add evaluation for positive integer inputs in javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 /*This file is for your custom js.  All yours*/
+//business logic
+
+var leapYear = function(year) {
+  if (year % 4 === 0 && year % 100 !== 0 || year % 400 = 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
