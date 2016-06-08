fd7b8f2a8cf7a18eb4de844bf77752391cce48c4
Kevin VanEvery and Matthew Giem <matthew@>
2016-06-08 08:34:18 -0700

    add validation for bad inputs and add output to html

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,32 @@
 //business logic
 
 var leapYear = function(year) {
-  if (year % 4 === 0 && year % 100 !== 0 || year % 400 = 0) {
+  if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
     return true;
   }
   else {
     return false;
   }
 }
+
+// user interface
+$(function () {
+  $("form#year-input").submit(function (event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    if (year <= 0) {
+      alert("Please enter a positive integer");
+      $(".year").addClass("has-error");
+    }
+    else {
+      leapYear(year);
+      if (leapYear) {
+        $("#output").html("<p>" + year + " is a leap year</p>")
+      }
+      else {
+        $("#output").html("<p>" + year + " is NOT a leap year</p>")
+      }
+    }
+
+  })
+})
